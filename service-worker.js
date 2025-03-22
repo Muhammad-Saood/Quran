3f7aadfdf39c5c38bd3c7795c43558ca147a181a
saudking48 <saud20759@gmail.com>
2025-03-22 06:36:02 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,41 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = "quran-pwa-cache";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/script.js",
+    "/icon-192x192.png",
+    "/icon-512x512.png"
+];
+
+// Install Service Worker and Cache Files
+self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open('Quran-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/script.js',
-                '/icon-192x192.png',
-                '/icon-512x512.png'
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Fetch Requests from Cache
+self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
